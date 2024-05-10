a7b6482f74f58d0ec0c8c7cda88426dc607cb774
ZhaoHaotian5 <zhaohaotian990210@126.com>
2024-05-10 10:10:00 +0800

    Hotsus

--- App/config.h
+++ App/config.h
@@ -4,14 +4,14 @@
 #include "parameters.h"
 
 // Debug
-#define DEBUG_BASIC true   // Print basic messages
-#define DEBUG_HELP true	   // Print help messages
-#define DEBUG_SERVER true  // Print server messages
-#define DEBUG_CLIENT true  // Print client messages
-#define DEBUG_MODULES true // Print modules messages
-#define DEBUG_TEE true	   // Print TEE messages
-#define DEBUG_LOG true	   // Print log messages
-#define DEBUG_TIME true	   // Print time messages
+#define DEBUG_BASIC true	// Print basic messages
+#define DEBUG_HELP false	// Print help messages
+#define DEBUG_SERVER false	// Print server messages
+#define DEBUG_CLIENT false	// Print client messages
+#define DEBUG_MODULES false // Print modules messages
+#define DEBUG_TEE false		// Print TEE messages
+#define DEBUG_LOG false		// Print log messages
+#define DEBUG_TIME false	// Print time messages
 
 #define MAXLINE 256
 #define MAX_SIZE_PAYLOAD 4096
